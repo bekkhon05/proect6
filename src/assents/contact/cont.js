084f35b38610ce8201d7c1e68b8331fd340ed772
azizbekcodemy <azizbekobidiov0303@gmail.com>
2023-02-19 08:44:55 +0300

    pageslar toplami

---
+++ src/assents/contact/cont.js
@@ -0,0 +1,28 @@
+import { FaPhoneAlt,FaEnvelope } from 'react-icons/fa';
+import {MdLocationOn } from 'react-icons/md';
+import {CiClock2 } from 'react-icons/ci';
+
+
+
+export const ConTactMap = [
+    {
+       icon:<FaPhoneAlt/>,
+       name:"Phone",
+       text:"+01-3-8888-6868"
+    },
+    {
+        icon:<MdLocationOn/>,
+        name:"Address",
+        text:"60-49 Road 11378 New York"
+     },
+     {
+        icon:<CiClock2/>,
+        name:"Open time",
+        text:"10:00 am to 23:00 pm"
+     },
+     {
+        icon:<FaEnvelope/>,
+        name:"Email",
+        text:"hello@colorlib.com"
+     },
+]
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
+import Blog from "../pages/Pageslar/Blog";
+import Detalis from "../pages/Pageslar/Detalis";
+import ChecOut from "../pages/Pageslar/Checaut";
+import Shopcard from "../pages/Pageslar/ShoppingCard";
+import Contact from "../pages/Contact";
 
 
 const Router = () => {
   return (
-    // <Routes>
-
-    //   <Route path="/" element={<Home />} />
-
-    // </Routes>
+    <Routes>
+       <Route path="/blog" element={<Blog />} />
+       <Route path="/detalis" element={<Detalis />} />
+       <Route path="/checout" element={<ChecOut />} />
+       <Route path="/shopcard" element={<Shopcard />} />
+       <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 };
 
